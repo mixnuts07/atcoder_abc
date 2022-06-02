adf0bb7615a7f1f13ab9b0864cc6dfc1d898d1e7
Kazutake Yamamoto <88083304+mixnuts07@users.noreply.github.com>
2022-06-02 18:20:51 +0900

    Update 245.py

--- 245.py
+++ 245.py
@@ -1,13 +1,13 @@
-# #(A)
-# a, b, c, d = input().split()
-# Takahashi = a+b
-# Aoki = c+d+str(1)
-# print("Takahashi" if Takahashi < Aoki else "Aoki")
+#(A)
+a, b, c, d = input().split()
+Takahashi = a+b
+Aoki = c+d+str(1)
+print("Takahashi" if Takahashi < Aoki else "Aoki")
 
-# #(B)
-# n = int(input())
-# An =set(list(map(int,input().split())))
-# for _ in range(0,n+1):
-#     if _ not in An:
-#         print(_)
-#         break
+#(B)
+n = int(input())
+An =set(list(map(int,input().split())))
+for _ in range(0,n+1):
+    if _ not in An:
+        print(_)
+        break
