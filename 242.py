c3004f6a769d8c31514e7e4dbc008ca877262173
Kazutake Yamamoto <88083304+mixnuts07@users.noreply.github.com>
2022-06-02 18:18:51 +0900

    Update 242.py

--- 242.py
+++ 242.py
@@ -1,12 +1,12 @@
-# # A
-# def Tshirt():
-#     A, B, C, X = map(int, input().split())
-#     if X <= A:
-#         return 1
-#     if A < X <= B:
-#         return C / (B - A)
-#     return 0
-# print(Tshirt())
-# # B
-# S = list(input())
-# print(('').join(sorted(S)))
+# A
+def Tshirt():
+    A, B, C, X = map(int, input().split())
+    if X <= A:
+        return 1
+    if A < X <= B:
+        return C / (B - A)
+    return 0
+print(Tshirt())
+# B
+S = list(input())
+print(('').join(sorted(S)))
